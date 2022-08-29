1de0f7dd02bd9d1b50f30128f61defec0b367906
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-08-29 11:50:55 +0200

    Incorporating CesiumJS in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+import { Ion, Viewer, createWorldTerrain, createOsmBuildings, Cartesian3, Math } from 'cesium';
+import 'cesium/Widgets/widget.css';
+import '../src/css/main.css';
+
+Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNWM5MTIwMS04M2I2LTQ0MDktYTdiYS1lOTU5OTVhN2QwYWQiLCJpZCI6OTk4OTAsImlhdCI6MTY1Njg3NjYxM30.qE_VIA2pTcJRNilSTTV4C3jZ4REYyutxVObRTr6ov_8';
+
+const viewer = new Viewer('cesiumContainer', {
+  terrainProvider: createWorldTerrain(),
+})
+
+viewer.scene.primitives.add(createOsmBuildings());
+
+viewer.camera.flyTo({
+  destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400.0),
+  orientation: {
+    heading: Math.toRadians(0.0),
+    pitch: Math.toRadians(-15.0),
+  }
+})
