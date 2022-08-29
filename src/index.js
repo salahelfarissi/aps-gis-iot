a005655b6dd133248315909d9992b0258944415f
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-08-29 11:58:47 +0200

    CesiumJS viewer working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Ion, Viewer, createWorldTerrain, createOsmBuildings, Cartesian3, Math } from 'cesium';
-import 'cesium/Widgets/widget.css';
+import 'cesium/Widgets/widgets.css';
 import '../src/css/main.css';
 
 Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNWM5MTIwMS04M2I2LTQ0MDktYTdiYS1lOTU5OTVhN2QwYWQiLCJpZCI6OTk4OTAsImlhdCI6MTY1Njg3NjYxM30.qE_VIA2pTcJRNilSTTV4C3jZ4REYyutxVObRTr6ov_8';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const cesiumSource = './node_modules/cesium/Source';
+const cesiumSource = 'node_modules/cesium/Source';
 const cesiumWorkers = '../Build/Cesium/Workers';
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
@@ -38,16 +38,16 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: 'src/index.html'
     }),
     // Copy Cesium Assets, Widgets, and Workers to a static directory
-    new CopyWebpackPlugin([{
+    new CopyWebpackPlugin({
       patterns: [
         { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
         { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
         { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }
       ]
-    }]),
+    }),
     new webpack.DefinePlugin({
       // Define relative base path in cesium for loading assets
       CESIUM_BASE_URL: JSON.stringify('')
