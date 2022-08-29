5985c919edb2c85a995880619db778aafa8097a6
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-08-29 10:37:29 +0200

    Installed required packages for loading CSS...

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,13 @@
 const path = require('path');
+
+const webpack = require('webpack');
+
+module.exports = {
+  // context specifies the directory where webpack should start looking for files to bundle
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+}
