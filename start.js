a19244b9863518aae97eacbccdb1a7806a87b469
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-08-29 09:53:07 +0200

    Using dist instead of public

--- start.js
+++ start.js
@@ -7,11 +7,11 @@ const express = require('express');
 const app = express();
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + 'public');
+  response.sendFile(__dirname + 'dist');
 });
 
 // listen for requests :)
