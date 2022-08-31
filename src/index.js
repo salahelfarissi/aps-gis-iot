812f63013aed88f3b73747239fa1e91fcf3acece
salaheddine <salaheddineelfarissi@gmail.com>
2022-08-31 11:06:55 +0200

    Removed module declaration

--- src/index.js
+++ src/index.js
@@ -3,8 +3,3 @@ import './css/style.css'
 
 // Log message to console
 logMessage('A very warm welcome to Expack!')
-
-// Needed for Hot Module Replacement
-if(typeof(module.hot) !== 'undefined') {
-  module.hot.accept() // eslint-disable-line no-undef  
-}
