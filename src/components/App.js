4f2ed0718359497e4c417553525345be8ac30855
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-12 15:56:55 +0200

    Added Ion Access token

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
-import { Cartesian3, createWorldTerrain } from 'cesium';
+import { Cartesian3, createWorldTerrain, Ion } from 'cesium';
 import React from 'react';
+
+Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNWM3NTk1Mi1hMTc5LTQ2NGUtODU4Ny1jODcwNmNmMjYzOTgiLCJpZCI6ODM0OTUsImlhdCI6MTY2Mjk4NjQzNn0.Pt87ii8UUSPnrgp2ngk1f1ELd2eHVWnE1rRhJ7PU4e0';
 
 const terrainProvider = createWorldTerrain();
 const position = Cartesian3.fromDegrees(2.2851128, 48.8305753, 100);
