fa62903cac879a70a236181f55d36aa51b8bd89c
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-12 20:18:57 +0200

    Explaining what html-webpack plugin is doing

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const webpack = require('webpack');
+// To avoid modifying html file manually, we use HtmlWebpackPlugin
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
