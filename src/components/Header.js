b9f23d430577ea4db4000ba3386cbf2bc724c831
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-13 21:18:34 +0200

    Navbar properly displayed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import {
-  Container, Row, Col, Navbar, Nav, NavLink, NavItem
+  Container, Row, Col, Navbar, Nav, NavLink, NavItem, NavbarBrand
 } from 'reactstrap';
 
+import avatar from '../assets/avatar.png';
 import logo from '../assets/logo.png';
 
 const Header = () => (
@@ -18,7 +19,7 @@ const Header = () => (
 
               <NavItem className="d-flex align-items-center">
                 <NavLink className="font-weight-bold" href="/">
-                  <img src={logo} alt="logo" className="img-fluid rounded-circle" style={{ width: 66 }} />
+                  <img src={avatar} alt="avatar" className="img-fluid rounded-circle" style={{ width: 36 }} />
                 </NavLink>
               </NavItem>
 
@@ -26,6 +27,20 @@ const Header = () => (
                 <NavLink className="font-weight-bold" href="/">Home</NavLink>
               </NavItem>
 
+            </Nav>
+          </Col>
+
+          <Col className="d-flex justify-content-xs-start justify-content-lg-center">
+            <NavbarBrand className="d-inline-block p-0" href="/" style={{ width: 80 }}>
+              <img src={logo} alt="logo" className="position-relative img-fluid" />
+            </NavbarBrand>
+          </Col>
+
+          <Col className="d-none d-lg-flex justify-content-end">
+            <Nav className="mrx-auto" navbar>
+              <NavItem className="d-flex align-items-center">
+                <NavLink className="font-weight-bold" href="/">About</NavLink>
+              </NavItem>
             </Nav>
           </Col>
 
