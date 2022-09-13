d1a56e0c9e4541f3848279926ed9fba8896aa871
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-13 16:45:17 +0200

    Added Header

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {
+  Container, Row, Col, Navbar, Nav, NavLink, NavItem
+} from 'reactstrap';
+
+const Header = () => (
+  <header>
+    <Navbar fixed="top" color="light" light expand="xs" className="border-bottom border-gray bg-white" style={{ height: 80 }}>
+
+      <Container>
+        <Row noGutters className="position-relative w-100 align-items-center">
+
+          <Col className="d-none d-lg-flex justify-content-start">
+            <Nav className="mrx-auto" navbar>
+
+              <NavItem className="d-flex align-items-center">
+                <NavLink className="font-weight-bold" href="/">Home</NavLink>
+              </NavItem>
+
+            </Nav>
+          </Col>
+
+        </Row>
+      </Container>
+
+    </Navbar>
+  </header>
+);
+
+export default Header;
