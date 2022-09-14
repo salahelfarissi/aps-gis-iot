246dccc58366ccfbf62e940b7ce454adbf60907e
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-14 11:33:58 +0200

    Moved viewer to stand alone component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
-import { Cartesian3, createWorldTerrain, Ion } from 'cesium';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from './components/Header';
-
-Ion.defaultAccessToken = process.env.ACCESS_TOKEN;
-
-const terrainProvider = createWorldTerrain();
-const position = Cartesian3.fromDegrees(2.2851128, 48.8305753, 100);
+import ModelViewer from './components/ModelViewer';
 
 function App() {
   return (
     <Router>
       <Header />
-      <Viewer full terrainProvider={terrainProvider}>
-        <Entity position={position} name='Ligne 12 (Porte de Versailles)'>
-          <PointGraphics pixelSize={10} />
-          <EntityDescription>
-            <h1>Auscultation des ouvrages souterrains</h1>
-          </EntityDescription>
-        </Entity>
-      </Viewer>
+      <main>
+        <ModelViewer />
+      </main>
     </Router>
   );
 }
