da022896b90b8bd2e9fe922b6ebf2ec901e22e8a
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-14 10:11:27 +0200

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
 import { Cartesian3, createWorldTerrain, Ion } from 'cesium';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from './components/Header';
 
@@ -11,7 +12,7 @@ const position = Cartesian3.fromDegrees(2.2851128, 48.8305753, 100);
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Viewer full terrainProvider={terrainProvider}>
         <Entity position={position} name='Ligne 12 (Porte de Versailles)'>
@@ -21,7 +22,7 @@ function App() {
           </EntityDescription>
         </Entity>
       </Viewer>
-    </>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -12,26 +12,26 @@ const Header = () => (
     <Navbar fixed="top" color="light" light expand="xs" className="border-bottom border-gray bg-white" style={{ height: 80 }}>
 
       <Container>
-        <Row noGutters className="position-relative w-100 align-items-center">
+        <Row className="position-relative w-100 align-items-center g-0">
 
           <Col className="d-none d-lg-flex justify-content-start">
             <Nav className="mrx-auto" navbar>
 
               <NavItem className="d-flex align-items-center">
-                <NavLink className="font-weight-bold" href="/">
+                <NavLink className="font-weight-bold" to="/">
                   <img src={avatar} alt="avatar" className="img-fluid rounded-circle" style={{ width: 36 }} />
                 </NavLink>
               </NavItem>
 
               <NavItem className="d-flex align-items-center">
-                <NavLink className="font-weight-bold" href="/">Home</NavLink>
+                <NavLink className="font-weight-bold" to="/">Home</NavLink>
               </NavItem>
 
             </Nav>
           </Col>
 
           <Col className="d-flex justify-content-xs-start justify-content-lg-center">
-            <NavbarBrand className="d-inline-block p-0" href="/" style={{ width: 80 }}>
+            <NavbarBrand className="d-inline-block p-0" to="/" style={{ width: 80 }}>
               <img src={logo} alt="logo" className="position-relative img-fluid" />
             </NavbarBrand>
           </Col>
@@ -39,7 +39,7 @@ const Header = () => (
           <Col className="d-none d-lg-flex justify-content-end">
             <Nav className="mrx-auto" navbar>
               <NavItem className="d-flex align-items-center">
-                <NavLink className="font-weight-bold" href="/">About</NavLink>
+                <NavLink className="font-weight-bold" to="/">About</NavLink>
               </NavItem>
             </Nav>
           </Col>
