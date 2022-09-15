e1349437aa55741d8f957cd88c437377f1bf1e89
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-15 11:57:41 +0200

    Seperated front from api

--- packages/front/webpack.server.config.js
+++ packages/front/webpack.server.config.js
@@ -9,7 +9,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = (env, argv) => {
-  const SERVER_PATH = './packages/api/src/index.js';
+  const SERVER_PATH = './src/server.js';
 
   return ({
     entry: {
