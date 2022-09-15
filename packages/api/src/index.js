1381243b501d700f3a1f9680a8cc30c8dda17468
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-15 10:16:03 +0200

    Added route for creating a metric

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -7,6 +7,20 @@ const pool = require('./utils/db');
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.post('/metrics', async (req, res) => {
+  try {
+    const { timestamp, sensor_type, metric } = req.body;
+    const newMetric = await pool.query(
+      'INSERT INTO metrics (timestamp, sensor_type, metric) VALUES ($1, $2, $3) RETURNING *',
+      [timestamp, sensor_type, metric]);
+
+    res.json(newMetric.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 app.listen(5000, () => {
   console.log('Server is running on port 5000.');
 });
--- packages/api/src/utils/db.js
+++ packages/api/src/utils/db.js
@@ -1,24 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'iot',
+  password: '%D2a3#PsT',
+  port: 5432,
 });
 
-const getMetrics = () => {
-  return new Promise(function(resolve, reject) {
-    pool.query('SELECT * FROM metrics', (error, results) => {
-      if (error) {
-        reject(error);
-      }
-      resolve(results.rows);
-    });
-  });
-};
-
-module.exports = {
- getMetrics,
-};
+module.exports = pool;
