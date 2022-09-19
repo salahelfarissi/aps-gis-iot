811e201406bc031d560e57bb9d345302db01cc39
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-19 17:31:12 +0200

    Installed plotly.js-strict-dist-min

--- packages/front/src/components/ModelViewer.js
+++ packages/front/src/components/ModelViewer.js
@@ -2,17 +2,33 @@ import React from 'react';
 import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
 import { Cartesian3, createWorldTerrain, Ion } from 'cesium';
 import { Container, Row, Col } from 'reactstrap';
+import createPlotlyComponent from 'react-plotly.js/factory';
+import Plotly from 'plotly.js-strict-dist-min';
 
 Ion.defaultAccessToken = process.env.REACT_APP_ACCESS_TOKEN;
 
 const terrainProvider = createWorldTerrain();
 const position = Cartesian3.fromDegrees(2.2851128, 48.8305753, 100);
 
+const Plot = createPlotlyComponent(Plotly);
+
 const ModelViewer = () => (
   <Container className='mt-3'>
     <Row>
       <Col className='col-4 d-flex align-items-center'>
-        <h1 className='text-center mt-1'>Ligne 12 (Porte de Versailles)</h1>
+        <Plot
+          data={[
+            {
+              x: [1, 2, 3],
+              y: [2, 6, 3],
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: {color: 'red'},
+            },
+            {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+          ]}
+          layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+        />
       </Col>
       <Col className='col-8'>
         <Viewer
