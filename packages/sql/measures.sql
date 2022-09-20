f6c0157b719d35d82bb56cdd2919122a2ab10ddb
salahelfarissi <salaheddineelfarissi@gmail.com>
2022-09-20 10:54:52 +0200

    Changed metrics to measures

---
+++ packages/sql/measures.sql
@@ -0,0 +1,4 @@
+INSERT INTO measures (timestamp, sensor_type, measure) VALUES 
+('2022-05-19 00:06:56', 'STA', '{"displacement": 0.003}'),
+('2022-05-19 00:07:55', 'STA', '{"displacement": 0.004}'),
+('2022-05-19 00:08:57', 'STA', '{"displacement": 0.005}');
