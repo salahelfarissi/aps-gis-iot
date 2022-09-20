1a4d8fd014cdd878c15ab78a9e7e59274b13ad16
Salaheddine EL FARISSI <salaheddineelfarissi@gmail.com>
2022-09-20 15:00:13 +0200

    Fetching data

--- packages/front/src/components/Plot.js
+++ packages/front/src/components/Plot.js
@@ -1,20 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import createPlotlyComponent from 'react-plotly.js/factory';
 import Plotly from 'plotly.js-strict-dist-min';
 
 const Plot = createPlotlyComponent(Plotly);
 
-const Graph = () => (
-  <Plot
-    data={[
-      {
-        x: ['2022-05-19 00:06:56', '2022-05-19 00:07:55', '2022-05-19 00:08:57'],
-        y: [0.003, 0.004, 0.005],
-        mode: 'lines+markers',
-        type: 'scatter',
-      },
-    ]}
-    layout={ {width: 600, height: 340, title: 'Displacement of rails'} }
-  />
-)
+const Graph = () => {
+  const [data, setData] = useState([]);
+
+  const getMeasures = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/measures');
+      const jsonData = await response.json();
+
+      console.log(jsonData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getMeasures();
+  })
+
+  return (
+    <Plot
+      data={[
+        {
+          x: data,
+          y: data,
+          mode: 'lines',
+          type: 'scatter',
+          marker: { color: 'red' },
+        },
+      ]}
+      layout={{ width: 600, height: 340, title: 'Displacement of rails' }}
+    />
+  )
+}
 
 export default Graph;
