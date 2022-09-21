e3b880b54941976b91ad247c9a07a6c5a02b3b28
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-21 15:50:55 +0200

    Added a ScatterPlotContainer

--- packages/front/src/components/ScatterPlot.js
+++ packages/front/src/components/ScatterPlot.js
@@ -1,96 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import createPlotlyComponent from 'react-plotly.js/factory';
 import Plotly from 'plotly.js-strict-dist-min';
 
 const Plot = createPlotlyComponent(Plotly);
 
-const ScatterPlot = () => {
-
-  const [measures, setMeasures] = useState([]);
-
-  const getMeasures = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/measures');
-      const jsonData = await response.json();
-
-      setMeasures(jsonData);
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
-  const primaryColor = '#0082de';
-  const secondaryColor = '#005499';
-
-  useEffect(() => {
-    getMeasures();
-  }, []);
-
-  const transformData = (data) => {
-    const x = [];
-    const y = [];
-
-    data.forEach((measure) => {
-      x.push(measure.timestamp);
-      y.push(measure.displacement);
-    });
-
-    return { x, y };
-  }
-
-  const { x, y } = transformData(measures);
-
-  const data = [
-    {
-      x: x,
-      y: y,
-      mode: 'lines+markers',
-      type: 'scatter',
-      name: 'Displacement',
-      marker: {
-        color: secondaryColor,
-        size: 5,
-        opacity: 0.5,
-      },
-      line: {
-        color: primaryColor,
-      },
-      hovertemplate: '<b>Displacement</b> %{y:.3f} m<extra></extra><br><b>Time</b> %{x}',
-    },
-  ]
-
-  const layout = {
-      title: {
-        text: 'Displacement of rails',
-        font: {
-          family: 'Roboto, sans-serif',
-          size: 18,
-        },
-        xref: 'paper',
-        x: 0.05, 
-      },
-      font: {
-        family: 'Open Sans, sans-serif',
-      },
-      showlegend: true,
-      legend: {'orientation': 'h'},
-      xaxis: {
-        title: 'Time',
-        type: 'date',
-      },
-      yaxis: {
-        range: [0, 0.008],
-      },
-      hovermode: 'closest',
-      hoverlabel: { bgcolor: primaryColor },
-    }
-
-  const config = {
-    responsive: true,
-    scrollZoom: true,
-    displaylogo: false,
-  }
-
+export const ScatterPlot = ({
+  data,
+  layout,
+  config,
+}) => {
   return (
     <Plot
       data={data}
@@ -100,5 +18,3 @@ const ScatterPlot = () => {
     />
   )
 }
-
-export default ScatterPlot;
