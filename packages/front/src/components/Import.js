3172fc04971c5a5d46dcf56019a253d0668b975d
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-23 16:47:48 +0200

    Added import component

---
+++ packages/front/src/components/Import.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+export default function Import() {
+  return (
+    <Container>
+      <h1>Observation Importer</h1>
+    </Container>
+  );
+}
