17a1df38e6d585c4914385af1e8bb71b2e9e7edb
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-24 13:03:41 +0200

    Added handle data change

--- packages/front/src/components/Import.js
+++ packages/front/src/components/Import.js
@@ -5,10 +5,34 @@ export default function Import() {
   const [url, setUrl] = useState("http://localhost:8080/FROST-Server/v1.1/Datastreams(1)/Observations");
   const [data, setData] = useState("2019-03-14T10:07:00.000Z, 0.002");
 
-  const execute = () => {
-    setData("Loading...");
+  const handleDataChange = (e) => {
+    setData(e.target.value);
   };
 
+  const execute = () => {
+    processData(url, data);
+  };
+
+  const processData = (url, data) => {
+    let allLines = data.split(/\r\n|\n/);
+    console.log(allLines);
+
+    for (let i = 0; i < allLines.length; i++) {
+      setData(allLines[i].split(','));
+      console.log(data.length);
+      if (data.length === 2) {
+          let observation = {
+              phenomenonTime: data[0].trim(),
+              result: data[1].trim()
+          };
+          post(url, JSON.stringify(observation));
+      }
+    }
+  };
+
+  const post = (url, data) => {
+    console.log("Posting to " + url + " with data " + data);
+  };
 
   return (
     <Container className="mt-3">
@@ -42,7 +66,7 @@ export default function Import() {
           name="content"
           id="data"
           style={{ fontFamily: 'Open Sans, sans-serif', fontSize: 13, height: 300 }}
-          onChange={e => e.target.value}
+          onChange={handleDataChange}
           value={data}
         />
       </Row>
