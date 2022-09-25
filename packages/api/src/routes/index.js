18165a9b760e25c5096771da95b17dc576205988
salahelfarissi <salaheddineelfarissi@gmail.com>
2022-09-25 19:28:23 +0200

    oss, buckets route working

--- packages/api/src/routes/index.js
+++ packages/api/src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import measures from "./measures";
 import oauth from "./forge/oauth";
+import oss from "./forge/oss";
 
 const config = require('../forge.config');
 if (config.credentials.client_id == null || config.credentials.client_secret == null) {
@@ -12,5 +13,6 @@ const router = Router();
 
 router.use("/measures", measures);
 router.use("/forge/oauth", oauth);
+router.use("/forge/oss", oss);
 
 export default router;
