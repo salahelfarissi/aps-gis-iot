8a02d4f75f53119a383cf4d7da5e3f12199a6fdc
salahelfarissi <salaheddineelfarissi@gmail.com>
2022-09-25 19:53:03 +0200

    Added model derivative route

--- packages/api/src/routes/index.js
+++ packages/api/src/routes/index.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import measures from "./measures";
 import oauth from "./forge/oauth";
 import oss from "./forge/oss";
+import modelderivative from "./forge/modelderivative";
 
 const config = require('../forge.config');
 if (config.credentials.client_id == null || config.credentials.client_secret == null) {
@@ -14,5 +15,6 @@ const router = Router();
 router.use("/measures", measures);
 router.use("/forge/oauth", oauth);
 router.use("/forge/oss", oss);
+router.use("/forge/modelderivative", modelderivative);
 
 export default router;
