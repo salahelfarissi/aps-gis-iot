6fc40fe8cc7d259bcccda56700ce0317ec2b2ded
salahelfarissi <salaheddineelfarissi@gmail.com>
2022-09-25 19:23:19 +0200

    Structuring routes for forge

--- packages/api/src/routes/index.js
+++ packages/api/src/routes/index.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
 import measures from "./measures";
-import forge from "./forge";
+import oauth from "./forge/oauth";
+
+const config = require('../forge.config');
+if (config.credentials.client_id == null || config.credentials.client_secret == null) {
+    console.error('Missing FORGE_CLIENT_ID or FORGE_CLIENT_SECRET env. variables.');
+}
 
 const router = Router();
 
 router.use("/measures", measures);
-router.use("/forge", forge);
+router.use("/forge/oauth", oauth);
 
 export default router;
