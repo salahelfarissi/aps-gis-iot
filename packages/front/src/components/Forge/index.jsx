f1e034e8632045a9bed9b3bae49ac865c7690e33
salahelfarissi <salaheddineelfarissi@gmail.com>
2022-09-26 19:04:38 +0200

    removed bootstrap link and script

--- packages/front/src/components/Forge/index.jsx
+++ packages/front/src/components/Forge/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+// import { Viewer } from "forge-dataviz-iot-react-components";
 
 const Forge = () => {
 
   return (
-    <div>
-      <h1>Forge</h1>
-    </div>
+    // <Viewer />
+    <h1>Hello</h1>
   );
 }
 
