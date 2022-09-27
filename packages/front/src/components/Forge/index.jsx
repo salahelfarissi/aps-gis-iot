799e512dba279cdea8aaf6a664a017b5224a9125
Salaheddine El Farissi <salaheddineelfarissi@gmail.com>
2022-09-27 15:29:53 +0200

    Forge viewer added

--- packages/front/src/components/Forge/index.jsx
+++ packages/front/src/components/Forge/index.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import ForgeViewer from 'forge-viewer-react';
-
-class Forge extends React.Component {
-
-  constructor(props) {
+import React, { Component } from 'react';
+import ForgeViewer from 'react-forge-viewer';
+import './App.css';
+ 
+class App extends Component {
+ 
+  constructor(props){
     super(props);
  
     this.state = {
       view:null
     }
   }
-
+ 
   handleViewerError(error){
     console.log('Error loading viewer.');
   }
-
+ 
+  /* after the viewer loads a document, we need to select which viewable to
+  display in our component */
   handleDocumentLoaded(doc, viewables){
     if (viewables.length === 0) {
       console.error('Document contains no viewables.');
@@ -24,7 +27,7 @@ class Forge extends React.Component {
       this.setState({view:viewables[0]});
     }
   }
-
+ 
   handleDocumentError(viewer, error){
     console.log('Error loading a document');
   }
@@ -36,24 +39,50 @@ class Forge extends React.Component {
   handleModelError(viewer, error){
     console.log('Error loading the model.');
   }
-
+ 
   getForgeToken(){
-    return fetch('http://localhost:3000/api/forge/oauth/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'client_id=YOUR_CLIENT_ID&client_secret=YOUR_CLIENT_SECRET&grant_type=client_credentials&scope=data:read'
-    }).then(res => res.json())
-    .then(res => res.access_token)
-    .catch(err => console.log(err));
-  }
+    let token = null;
 
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:5000/api/forge/oauth/token', false);
+    xhr.send(null);
+    if (xhr.status === 200) {
+      token = JSON.parse(xhr.responseText);
+    }
+    return token;
+  }
+ 
+  /* Once the viewer has initialized, it will ask us for a forge token so it can
+  access the specified document. */
+  handleTokenRequested(onAccessToken){
+    console.log('Token requested by the viewer.');
+    if(onAccessToken){
+      let token = this.getForgeToken();
+      console.log(token);
+      if(token)
+        onAccessToken(
+          token.access_token, token.expires_in);
+    }
+  }
+ 
   render() {
     return (
-      <h1>Hello</h1>
+      <div className="App">
+        <ForgeViewer
+          version="6.0"
+          urn="dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dGZlLmJ1Y2tldC90dW5uZWwucnZ0"
+          view={this.state.view}
+          headless={false}
+          onViewerError={this.handleViewerError.bind(this)}
+          onTokenRequest={this.handleTokenRequested.bind(this)}
+          onDocumentLoad={this.handleDocumentLoaded.bind(this)}
+          onDocumentError={this.handleDocumentError.bind(this)}
+          onModelLoad={this.handleModelLoaded.bind(this)}
+          onModelError={this.handleModelError.bind(this)}
+        />
+      </div>
     );
   }
 }
-
-export default Forge;
+ 
+export default App;
