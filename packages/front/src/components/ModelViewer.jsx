953b69cd12c9a0d5a06749617c43ed613424a23b
salahelfarissi <salaheddineelfarissi@gmail.com>
2022-10-13 11:14:27 +0200

    Removed unnecessary widgets

--- packages/front/src/components/ModelViewer.jsx
+++ packages/front/src/components/ModelViewer.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Viewer, Entity, PointGraphics, EntityDescription } from "resium";
+import PropTypes from "prop-types";
 
 export const ModelViewer = ({
   terrainProvider,
@@ -10,8 +10,19 @@ export const ModelViewer = ({
   description,
 }) => (
   <Viewer
+    baseLayerPicker={false}
+    homeButton={false}
+    animation={false}
+    scene3DOnly={true}
+    shadows={true}
+    fullscreenButton={false}
+    selectionIndicator={false}
+    // ! App crashes if infoBox is set
+    // infoBox={false}
     terrainProvider={terrainProvider}
-    className='cesium-container'>
+    timeline={false}
+    className="cesium-container"
+  >
     <Entity position={position} name={name}>
       <PointGraphics pixelSize={pixelSize} />
       <EntityDescription>
@@ -19,7 +30,7 @@ export const ModelViewer = ({
       </EntityDescription>
     </Entity>
   </Viewer>
-)
+);
 
 ModelViewer.propTypes = {
   terrainProvider: PropTypes.object.isRequired,
@@ -27,4 +38,4 @@ ModelViewer.propTypes = {
   name: PropTypes.string.isRequired,
   pixelSize: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
-}
+};
