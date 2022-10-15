ed1b6bfca3ff75cc7effbada7a9da9165dcdb436
salahelfarissi <salaheddineelfarissi@gmail.com>
2022-10-15 15:54:58 +0200

    Using native leaflet

--- packages/front/src/components/Map.jsx
+++ packages/front/src/components/Map.jsx
@@ -1,18 +1,79 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
 
 const Map = () => {
+  // Create the map ref:
+  const mapRef = useRef(null);
+  // Create the tile ref:
+  const tileRef = useRef(null);
+
+  // Create our map tile layer:
+  tileRef.current = L.tileLayer(
+    `https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`,
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+  );
+
+  // Define the styles that are to be passed to the map instance:
+  const mapStyles = {
+    overflow: "hidden",
+    width: "100%",
+    height: "52vh",
+  };
+
+  // Define an object literal with params that will be passed to the map:
+  useEffect(() => {
+    mapRef.current = L.map("map", {
+      center: [37.0902, -95.7129],
+      zoom: 3,
+      zoomControl: false,
+      maxBounds: L.latLngBounds(L.latLng(-150, -240), L.latLng(150, 240)),
+      layers: [tileRef.current],
+    });
+  }, []);
+
+  // Add controls:
+  useEffect(() => {
+    // Create the zoom control:
+    L.control
+      .zoom({
+        position: "topright",
+      })
+      .addTo(mapRef.current); // Add the control to our map instance
+  }, []);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src = "https://unpkg.com/sta-map@latest/dist/stam.min.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Use mapRef.current to add map event listeners to
+    // our map instance:
+    mapRef.current.on("zoomstart", () => {
+      console.log("ZOOM STARTED");
+      console.log(window);
+    });
+  }, []);
+
+  L.stam({
+    baseUrl:
+      "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+  });
+
   return (
     <>
-      <MapContainer
-        center={[48.8305753, 2.2851128]}
-        zoom={15}
-        scrollWheelZoom={true}
-      >
-        <TileLayer url="https://api.mapbox.com/styles/v1/salahelfarissi/cl96t7idm000l14mtg9tqgpfj/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FsYWhlbGZhcmlzc2kiLCJhIjoiY2ttb3p1Yzk3Mjl2bzJ2bno3OGlqcjJ2bCJ9.pErPZNgS_t5jzHlsp_XyRQ" />
-        <Marker position={[48.8305753, 2.2851128]}>
-          <Popup>Ligne 12 (Porte de Versailles)</Popup>
-        </Marker>
-      </MapContainer>
+      <div id="map" style={mapStyles}></div>
     </>
   );
 };
