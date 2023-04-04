d99df71592e5734ba22cc8e3b528de62bef508ef
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-04-04 14:09:48 +0200

    Work on plotly alone

--- packages/front/src/App.js
+++ packages/front/src/App.js
@@ -5,13 +5,14 @@ import { Container, Row, Col } from 'reactstrap';
 // Components
 import About from './components/About';
 import ImportDataContainer from './containers/ImportDataContainer';
-import ScatterPlotContainer from './containers/ScatterPlotContainer';
+// import ScatterPlotContainer from './containers/ScatterPlotContainer';
 import Header from './components/Header';
 // import ModelViewerContainer from './containers/ModelViewerContainer';
 // import Forge from './components/Forge';
-import APS from './components/APS';
+// import APS from './components/APS';
 import Map from './components/Map';
-import SceneViewer from './components/SceneViewer';
+// import SceneViewer from './components/SceneViewer';
+import ExampleComponent from './components/ExampleComponent';
 
 function App() {
   return (
@@ -24,15 +25,16 @@ function App() {
         <Row style={{ height: '52vh' }}>
           <Col>
             {/* <Route path="/" exact component={Forge} /> */}
-            <Route path="/" exact component={APS} />
+            {/* <Route path="/" exact component={APS} /> */}
           </Col>
           <Col>
             {/* <Route path="/" exact component={ModelViewerContainer} /> */}
-            <Route path="/" exact component={SceneViewer} />
+            {/* <Route path="/" exact component={SceneViewer} /> */}
           </Col>
         </Row>
         <Row style={{ height: '40vh' }}>
-          <Route path="/" exact component={ScatterPlotContainer} />
+          {/* <Route path="/" exact component={ScatterPlotContainer} /> */}
+          <Route path="/" exact component={ExampleComponent} />
         </Row>
       </Container>
     </Router>
